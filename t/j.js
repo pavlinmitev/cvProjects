b897e11983adf654c559410470858a19eae3d2f4
pavlinmitev <40584075+pavlinmitev@users.noreply.github.com>
2019-09-21 19:49:51 +0300

    Delete j.js

--- t/j.js
+++
@@ -1,9 +0,0 @@
-var el=document.getElementById("bars");
-el.addEventListener("click",(function(){
-    var el1=document.getElementById("bar1");
-    var el2=document.getElementById("bar2");
-    var el3=document.getElementById("bar3");
-    el1.classList.toggle("class1");
-    el2.classList.toggle("class3");
-   el3.classList.toggle("class2");
-}));
